75917b13c5609458632a3cacc9193283bf915bb6
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-02-04 22:34:34 -0800

    ready to merge with backend

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,36 +1,12 @@
 import React from 'react'
-import Navbar from '../Navbar';
-import Board from '../Board';
-import Button from '../Button';
-
+import Navbar from './Navbar';
 
 
 const Home = () => {
-  const source = [
-    '../../images/home01.jpeg',
-    '../../images/home02.jpeg',
-    '../../images/home03.jpeg',
-    '../../images/home04.jpeg',
-    '../../images/home05.jpeg',
-    '../../images/home06.jpeg',
-    '../../images/home07.jpeg',
-    '../../images/home08.jpeg',
-    '../../images/home09.jpeg'
-  ];
-
-
-  function handleImageClick(i) {
-    console.log("Home: " + i + " clicked");
-  }
-
-  return (
-    <>
-      <Navbar />
-        <Board path={source} handleImageClick={handleImageClick} />
-        <Button />
-      <div></div>
-    </>
-  )
+  <>
+    <Navbar />
+    <div></div>
+  </>
 }
 
 export default Home
