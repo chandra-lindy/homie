099b9e2b3cce197c1f19ffc271e543719c3b627a
Chandra Lindy <chandra.lindy@csu.fullerton.edu>
2023-02-04 18:24:04 -0800

    home images displayed in three by three format; click from each image handled correctly

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Navbar from '../Navbar';
+import Board from '../Board';
+
+
 
 const Home = () => {
+  const source = [
+    '../../images/home01.jpeg',
+    '../../images/home02.jpeg',
+    '../../images/home03.jpeg',
+    '../../images/home04.jpeg',
+    '../../images/home05.jpeg',
+    '../../images/home06.jpeg',
+    '../../images/home07.jpeg',
+    '../../images/home08.jpeg',
+    '../../images/home09.jpeg'
+  ];
+
+
+  function handleImageClick(i) {
+    console.log("Home: " + i + " clicked");
+  }
+
   return (
     <>
-    <Navbar/>
-    <div></div>
+      <Navbar />
+        <Board path={source} handleImageClick={handleImageClick} />
+      <div></div>
     </>
   )
 }
